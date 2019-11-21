793ad304c57c8fa20e84e821bb04167d2e85efa9
Francis <frockwel@usc.edu>
2019-11-21 13:10:20 -0800

    added in-game sliders to adjust metallicness and smoothness

--- Assets/Scripts/FileOpener.cs
+++ Assets/Scripts/FileOpener.cs
@@ -7,10 +7,19 @@ using SFB;
 
 public class FileOpener : MonoBehaviour
 {
+
+    Shader myShader;
+
+    Renderer rend;
+
+    public GameObject teapot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myShader = Shader.Find("Physically-Based-Lighting");
+        teapot = GameObject.FindGameObjectWithTag("Player");
+        rend = teapot.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -28,14 +37,37 @@ public class FileOpener : MonoBehaviour
         var extensions = new[] {
             new ExtensionFilter("Image Files", "png", "jpg", "jpeg" ),
         };
-        Texture2D texture = GameObject.FindGameObjectWithTag("Player").GetComponent<Renderer>().material.mainTexture as Texture2D;
+        Texture2D texture = rend.material.mainTexture as Texture2D;
 
         var path = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, true);
         var fileContent = File.ReadAllBytes(path[0]);
 
         texture.LoadImage(fileContent);
 
+        rend.material = new Material(myShader);
+        rend.material.color = Color.blue;
+        rend.material.mainTexture = texture;
+
 
 
     }
+
+    public void UpdateMetallic(float val)
+    {
+        if(rend)
+            rend.material.SetFloat("_Metallic", val);
+    }
+
+    public void UpdateSmoothness(float val)
+    {
+        if(rend)
+            rend.material.SetFloat("_Glossiness", val);
+    }
 }
+/*
+ * 
+ * Material newMat = rend.material;
+        newMat.mainTexture = texture;
+
+        rend.material.shader = myShader;
+*/
