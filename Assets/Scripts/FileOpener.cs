03e57153155acbce57eeaa5dbcfb7071a1bd66e9
Francis <frockwel@usc.edu>
2019-11-26 16:50:39 -0800

    Added more sliders and two more teapots

--- Assets/Scripts/FileOpener.cs
+++ Assets/Scripts/FileOpener.cs
@@ -10,16 +10,20 @@ public class FileOpener : MonoBehaviour
 
     Shader myShader;
 
-    Renderer rend;
+    Renderer rend0, rend1, rend2;
 
-    public GameObject teapot;
+    public GameObject obj0, obj1, obj2;
 
     // Start is called before the first frame update
     void Start()
     {
         myShader = Shader.Find("Physically-Based-Lighting");
-        teapot = GameObject.FindGameObjectWithTag("Player");
-        rend = teapot.GetComponent<Renderer>();
+        obj0 = GameObject.FindGameObjectWithTag("Player");
+        obj1 = GameObject.FindGameObjectWithTag("Player1");
+        obj2 = GameObject.FindGameObjectWithTag("Player2");
+        rend0 = obj0.GetComponent<Renderer>();
+        rend1 = obj1.GetComponent<Renderer>();
+        rend2 = obj2.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -37,16 +41,16 @@ public class FileOpener : MonoBehaviour
         var extensions = new[] {
             new ExtensionFilter("Image Files", "png", "jpg", "jpeg" ),
         };
-        Texture2D texture = rend.material.mainTexture as Texture2D;
+        //Texture2D texture = rend.material.mainTexture as Texture2D;
 
-        var path = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, true);
-        var fileContent = File.ReadAllBytes(path[0]);
+        //var path = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, true);
+        //var fileContent = File.ReadAllBytes(path[0]);
 
-        texture.LoadImage(fileContent);
+        //texture.LoadImage(fileContent);
 
-        rend.material = new Material(myShader);
-        rend.material.color = Color.blue;
-        rend.material.mainTexture = texture;
+        //rend.material = new Material(myShader);
+        //rend.material.color = Color.blue;
+        //rend.material.mainTexture = texture;
 
 
 
@@ -54,14 +58,65 @@ public class FileOpener : MonoBehaviour
 
     public void UpdateMetallic(float val)
     {
-        if(rend)
-            rend.material.SetFloat("_metallic", val);
+        rend0.material.SetFloat("_metallic", val);
+        rend1.material.SetFloat("_metallic", val);
+        rend2.material.SetFloat("_metallic", val);
     }
 
     public void UpdateSmoothness(float val)
     {
-        if(rend)
-            rend.material.SetFloat("_roughness", val);
+        rend0.material.SetFloat("_roughness", val);
+        rend1.material.SetFloat("_roughness", val);
+        rend2.material.SetFloat("_roughness", val);
+    }
+
+    public void UpdateSubSurface(float val)
+    {
+        rend0.material.SetFloat("_subSurface", val);
+        rend1.material.SetFloat("_subSurface", val);
+        rend2.material.SetFloat("_subSurface", val);
+    }
+
+    public void UpdateSpecular(float val)
+    {
+        rend0.material.SetFloat("_specular", val);
+        rend1.material.SetFloat("_specular", val);
+        rend2.material.SetFloat("_specular", val);
+    }
+
+    public void UpdateSpecularTint(float val)
+    {
+        rend0.material.SetFloat("_specularTint", val);
+        rend1.material.SetFloat("_specularTint", val);
+        rend2.material.SetFloat("_specularTint", val);
+    }
+
+    public void UpdateAnisotropic(float val)
+    {
+        rend0.material.SetFloat("_anIsotropic", val);
+        rend1.material.SetFloat("_anIsotropic", val);
+        rend2.material.SetFloat("_anIsotropic", val);
+    }
+
+    public void UpdateSheen(float val)
+    {
+        rend0.material.SetFloat("_sheen", val);
+        rend1.material.SetFloat("_sheen", val);
+        rend2.material.SetFloat("_sheen", val);
+    }
+
+    public void UpdateSheenTint(float val)
+    {
+        rend0.material.SetFloat("_sheenTint", val);
+        rend1.material.SetFloat("_sheenTint", val);
+        rend2.material.SetFloat("_sheenTint", val);
+    }
+
+    public void UpdateClearCoatGloss(float val)
+    {
+        rend0.material.SetFloat("_clearCoatGloss", val);
+        rend1.material.SetFloat("_clearCoatGloss", val);
+        rend2.material.SetFloat("_clearCoatGloss", val);
     }
 }
 /*
